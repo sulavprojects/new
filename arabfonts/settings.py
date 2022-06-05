9465c17eb6b42b88a29bedbd0a0466c1f14865c9
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-05 14:25:38 +0545

    first commit

--- arabfonts/settings.py
+++ arabfonts/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'froala_editor',
+    'home',
 ]
 
 MIDDLEWARE = [
--- home/models.py
+++ home/models.py
@@ -1,3 +1,56 @@
+from turtle import title
 from django.db import models
+from froala_editor.fields import FroalaField
+from .helpers import *
 
 # Create your models here.
+class Fonts(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, null=True, blank=True)
+    metakeyword = models.CharField(max_length=200, null=True)
+    header_image = models.ImageField(upload_to='header_image')
+    upload_fonts = models.FileField(upload_to="upload_fonts")
+    short_dis = models.TextField()
+    long_dis = FroalaField()
+    char_maping = models.ImageField(upload_to='char_maping')
+    publish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self , *args, **kwargs):
+        self.slug = generate_slug(self.title)
+        super(Fonts, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return f'/{self.slug}'
+
+
+class Modification(models.Model):
+    websitename = models.CharField(max_length=50)
+    ouremail = models.EmailField()
+    copyright = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='websiteimages')
+    websitediscription = FroalaField()
+    favicon = models.ImageField()
+
+    def __str__(self):
+        return self.websitename
+
+
+class Pages(models.Model):
+    aboutus = FroalaField()
+    privacypolicy = FroalaField()
+    termsandcondition = FroalaField()
+    ourpages = FroalaField()
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return 'message from -' + self.name + ' - ' + self.email 
+
