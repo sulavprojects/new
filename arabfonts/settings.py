bd9fe68ef51665099e66b5c459cb3cdf7cd7beae
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-05 12:54:27 +0545

    first commit

--- arabfonts/settings.py
+++ arabfonts/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
     
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
