5cad364b990b59fa1cdecd430e77f218e814e001
django <78250295+iamsulavpariyar@users.noreply.github.com>
2022-06-05 14:33:54 +0545

    first commit

--- arabfonts/settings.py
+++ arabfonts/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'froala_editor',
+    # 'froala_editor',
     'home',
 ]
 
